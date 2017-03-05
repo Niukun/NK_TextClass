fda3fe395ee6d6ed26504d89b01f3f56b86667b9
Niukun <1464042197@qq.com>
2017-03-05 10:00:59 +0800

    清洗代码3.0

--- trunk/NK_TextClass_2.0/src/main/java/data/DataClean.java
+++ trunk/NK_TextClass_2.0/src/main/java/data/DataClean.java
@@ -24,9 +24,17 @@ public class DataClean {
 		cleanReading("C:/D/NLPIR/paper/files/test/reading/","C:/D/NLPIR/paper/files/testnum/reading/");
 		cleanSocietylaw("C:/D/NLPIR/paper/files/test/society&law/","C:/D/NLPIR/paper/files/testnum/society&law/");
 		
-//		System.out.println("train.......................");
+		System.out.println("train.......................");
 		
-		
+		cleanCulture("C:/D/NLPIR/paper/files/train/culture/","C:/D/NLPIR/paper/files/trainnum/culture/");
+		cleanEducation("C:/D/NLPIR/paper/files/train/education/","C:/D/NLPIR/paper/files/trainnum/education/");
+		cleanEntertainment("C:/D/NLPIR/paper/files/train/entertainment/","C:/D/NLPIR/paper/files/trainnum/entertainment/");
+		cleanHistory("C:/D/NLPIR/paper/files/train/history/","C:/D/NLPIR/paper/files/trainnum/history/");
+		cleanIT("C:/D/NLPIR/paper/files/train/it/","C:/D/NLPIR/paper/files/trainnum/it/");
+		cleanMilitary("C:/D/NLPIR/paper/files/train/military/","C:/D/NLPIR/paper/files/trainnum/military/");
+		cleanReading("C:/D/NLPIR/paper/files/train/reading/","C:/D/NLPIR/paper/files/trainnum/reading/");
+		cleanSocietylaw("C:/D/NLPIR/paper/files/train/society&law/","C:/D/NLPIR/paper/files/trainnum/society&law/");
+	
 		System.out.println("end....");
 	}
 
@@ -47,8 +55,16 @@ public class DataClean {
 				} else if (line.contains("最佳建仓股")) {
 				} else if (line.contains("资料图片：")) {
 				} else if (line.contains("我要评论")) {
+				} else if (line.contains("最美微笑战士")) {
+				} else if (line.contains("最美警花救起跳楼少女")) {
+				} else if (line.contains("2008中国最美50人")) {
+				} else if (line.contains("双胞胎大战中国出水芙蓉")) {
+				} else if (line.contains("成都最美环卫工")) {
+				} else if (line.contains("最美女大学生")) {
+				} else if (line.contains("最美酒模\"带小朋友")) {
 				} else if (line.contains("连线：")) {
 				} else if (line.contains("网上整理")) {
+				} else if (line.contains("上海植物园里的")) {
 				} else if (line.contains("延伸阅读")) {
 					break;
 				} else if (line.contains("欢迎登录")) {
@@ -87,7 +103,17 @@ public class DataClean {
 				} else if (line.contains("中国新闻出版报-网")) {
 				} else if (line.contains("已有评论")) {
 				} else if (line.contains("精彩预告")) {
+				} else if (line.contains("点击阅读")) {
 				} else if (line.contains("特色铃音")) {
+				} else if (line.contains("日期：")) {
+				} else if (line.contains("时间：")) {
+				} else if (line.contains("地点：")) {
+				} else if (line.contains("讲者：")) {
+				} else if (line.contains("报名：")) {
+				} else if (line.contains("民间野史")) {
+				} else if (line.contains("洪秀全败于极度淫乱")) {
+				} else if (line.contains("罗马皇帝却为娶她")) {
+				} else if (line.contains("享尽帝后之仪：")) {
 				} else if (line.contains("写成了")) {
 				} else if (line.contains("【】")) {
 				} else if (line.contains("发表：")) {
@@ -128,6 +154,8 @@ public class DataClean {
 				} else if (line.contains("更多精彩内容")) {
 				} else if (line.contains("更多内容阅读")) {
 				} else if (line.contains("专题回顾：")) {
+				} else if (line.contains("彩预告片")) {
+				} else if (line.contains("特色铃音")) {
 				} else if (line.contains("下一页")) {
 				} else if (line.contains("后一页")) {
 				} else if (line.contains("前一页")) {
@@ -218,6 +246,12 @@ public class DataClean {
 				} else if (line.contains("图片推荐频道头")) {
 				} else if (line.contains("匿名发表")) {
 				} else if (line.contains("本文来源:")) {
+				} else if (line.contains("来源:")) {
+				} else if (line.contains("地址:")) {
+				} else if (line.contains("邮箱:")) {
+				} else if (line.contains("电话:")) {
+				} else if (line.contains("传真:")) {
+				} else if (line.contains("奇趣音效")) {
 				} else if (line.contains("精彩预告片")) {
 				} else if (line.contains("中国远征军痛宰鬼子兵")) {
 				} else if (line.contains("BigNews")) {
@@ -356,6 +390,7 @@ public class DataClean {
 				} else if (line.contains("网友发表评论")) {
 				} else if (line.contains("参与评论匿名用户名")) {
 				} else if (line.contains("本文节选自")) {
+				} else if (line.contains("本文摘自")) {
 				} else if (line.contains("相关链接")) {
 					break;
 				} else if (line.contains("相关阅读")) {
